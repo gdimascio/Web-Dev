bc8656c03bb12e5b6f594f5d1e8874dca04fc208
Guido Di Mascio <106411980+gdimascio@users.noreply.github.com>
2022-08-28 11:37:44 -0300

    Add files via upload
    scroll-beh & top-btn

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,19 @@
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "inline-block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+
+
 function burger_menu(){
     var x = document.getElementById("_burger-links");
     if (x.style.display === "flex") {
@@ -6,3 +22,4 @@ function burger_menu(){
         x.style.display = "flex";
     }
 }
+
